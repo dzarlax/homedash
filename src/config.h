a8806c384d78b0a0ea15fb9481f3f300a9d22d3b
Dzarlax <me@dzarlax.dev>
2026-05-30 20:02:19 +0200

    Pin ESP-IDF platform for firmware release

--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // --- Firmware version (bump before each release) ---
-#define FW_VERSION  "1.0.2"
+#define FW_VERSION  "1.0.3"
 
 // --- Location ---
 #define WEATHER_CITY "Belgrade"
